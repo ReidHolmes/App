ae7cf2f0f76d726fbc95dae6df689f091d6d804a
ReidHolmes <name.reidholmes@gmail.com>
2019-02-21 13:58:00 +0800

    组件练习

--- Zujian/AppRukou_ViewZujian.js
+++ Zujian/AppRukou_ViewZujian.js
@@ -16,9 +16,12 @@ export default class AppRukou_ViewZujian extends Component {
     return (
       <View style={styles.container}>
 
-
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <View style={styles.item}>
+        <Text style={styles.center}>酒店</Text>
+        <Text style={styles.center}>To get started, edit App.js</Text>
+        </View>
+        <View style={styles.item}></View>
+        <View style={styles.item}></View>
       </View>
     );
   }
@@ -27,16 +30,16 @@ export default class AppRukou_ViewZujian extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    borderWidth:1,
+    borderColor:'red',
+    flexDirection:'row',
   },
-  welcome: {
+  item: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  center: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
