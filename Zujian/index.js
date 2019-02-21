02a45dba288b8dccb6b7283d0bf9d60ff0a3e8a5
ReidHolmes <name.reidholmes@gmail.com>
2019-02-21 12:59:31 +0800

    组件练习

--- Zujian/index.js
+++ Zujian/index.js
@@ -4,7 +4,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import ViewZujian from './ViewZujian';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => ViewZujian);
