5b174db3805eff6cc21eef02cdb0a3971257b13c
ReidHolmes <name.reidholmes@gmail.com>
2019-02-23 15:03:22 +0800

    初始化，（注：未完成）

--- Zujian/TouchableZu.js
+++ Zujian/TouchableZu.js
@@ -8,44 +8,36 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback,StyleSheet, Text, View, TextInput} from 'react-native';
 const Header=require('./header');
 export default class TouchableZu extends Component {
     render() {
         return (
-            <View style={styles.topStatus}>
-                <Search></Search>
+            <View style={styles.flex}>
+                <TouchableHighlight onPress={this.show.bind(this,'TouchableHighlight')}
+                    underlayColor='#E1F6FF'>
+
+                    <Text style={styles.item}>我是TouchableHighlight</Text>
+
+                    </TouchableHighlight>
+
+                    <TouchableOpacity onPress={this.show.bind(this,'TouchableOpacity')}
+                    underlayColor='#E1F6FF'>
+
+                    <Text style={styles.item}>我是TouchableOpacity</Text>
+
+                    </TouchableOpacity>
+                    <TouchableWithoutFeedback onPress={this.show.bind(this,'TouchableWithoutFeedback')}
+                    underlayColor='#E1F6FF'>
+
+                    <Text style={styles.item}>我是TouchableWithoutFeedbackt</Text>
+
+                    </TouchableWithoutFeedback>
             </View>
         );
     }
 }
 
-class Search extends Component {
-    render() {
-        return (
-            <View style={styles.flexDirection}>
-
-                <View style={styles.input}>
-                    <TextInput  returnKeyType='Search'/>
-                </View>
-
-
-                <View style={styles.btn}>
-
-                    <Text style={styles.search}>搜索</Text>
-
-                </View>
-
-
-
-
-
-
-
-            </View>
-        );
-    }
-}
 
 
 
@@ -53,15 +45,7 @@ const styles = StyleSheet.create({
     flex:{
         flex:1,
     },
-    topStatus:{
-        marginTop:25,
-        flex:1,
-    },
-    flexDirection:{
-        flexDirection:'row',
-        flex:1,
-    },
-    input:{
+    item:{
         flex:1,
         height:45,
         borderColor:'red',
@@ -69,21 +53,6 @@ const styles = StyleSheet.create({
         marginLeft:10,
         paddingLeft:10,
         borderRadius:5,
-    },
-    btn:{
-        width:45,
-        marginLeft:-5,
-        marginRight:5,
-        backgroundColor:'#23BEFF',
-        height:45,
-        justifyContent:'center',
-        alignItems:'center',
-        flex:1,
-    },
-    search:{
-        color:'#fff',
-        fontSize:15,
-        fontWeight:'bold',
     },
 
 });
